6a743ee0cafa88fe72b164381a28dc6ef975d80f
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2024-01-19 21:23:57 +0700

    source code landing page crypto coin

--- resources/views/livewire/like/index.blade.php
+++
@@ -1,8 +0,0 @@
-<div>
-    <form style="margin-top: -5px;">
-        <button
-            @if (isset($statusLikeOrNot) && $statusLikeOrNot->status) wire:click.prevent="delete()" @else wire:click.prevent="store()" @endif>
-            <i class="fas fa-star{{ isset($statusLikeOrNot) && $statusLikeOrNot->status ? ' text-warning' : '' }}"></i>
-        </button>
-    </form>
-</div>
